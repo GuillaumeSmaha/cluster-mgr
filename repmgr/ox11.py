1b51f65ecccc0121ca50fc9a48324c66b054efcb
Isman Firmansyah <isman.firmansyah@gmail.com>
2017-04-02 17:57:34 +0700

    fix ox11 module

--- repmgr/ox11.py
+++ repmgr/ox11.py
@@ -3,24 +3,30 @@ import requests
 GENERATE_KEY_ENDPOINT = '/oxeleven/rest/oxeleven/generateKey'
 DELETE_KEY_ENDPOINT = '/oxeleven/rest/oxeleven/deleteKey'
 
-def generate_key(ox11server='http://localhost:8190', signature_algorithm='RS512', expiration_time='216000', token='d-u-m-m-y'):
+
+def generate_key(ox11server='http://localhost:8190',
+                 signature_algorithm='RS512',
+                 expiration_time='216000',
+                 token='d-u-m-m-y'):
     url = ox11server + GENERATE_KEY_ENDPOINT
-    payload = {'signatureAlgorithm': signature_algorithm, 'expirationTime': expiration_time}
+    payload = {'signatureAlgorithm': signature_algorithm,
+               'expirationTime': expiration_time}
     headers = {'Authorization': 'Bearer {}'.format(token)}
     r = requests.post(url, data=payload, headers=headers, verify=False)
-    try:
-        out = r.json()
-    except:
-        out = {}
-    return r.status_code, out
 
-def delete_key(ox11server='http://localhost:8190', kid='d-u-m-m-y', token='d-u-m-m-y'):
+    if not r.ok:
+        return r.status_code, r.text
+    return r.status_code, r.json()
+
+
+def delete_key(ox11server='http://localhost:8190',
+               kid='d-u-m-m-y',
+               token='d-u-m-m-y'):
     url = ox11server + DELETE_KEY_ENDPOINT
     payload = {'kid': kid}
     headers = {'Authorization': 'Bearer {}'.format(token)}
     r = requests.post(url, data=payload, headers=headers, verify=False)
-    try:
-        out = r.json()
-    except:
-        out = {}
-    return r.status_code, out
+
+    if not r.ok:
+        return r.status_code, r.text
+    return r.status_code, r.json()
