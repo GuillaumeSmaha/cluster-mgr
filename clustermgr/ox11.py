a6ea090e10286f043d088d35a5f127060cff9617
Isman Firmansyah <isman.firmansyah@gmail.com>
2017-05-24 03:14:17 +0700

    conform to oxEleven expirationTime format

--- clustermgr/ox11.py
+++ clustermgr/ox11.py
@@ -1,16 +1,25 @@
+import time
+from datetime import timedelta
+
 import requests
 
 GENERATE_KEY_ENDPOINT = '/oxeleven/rest/oxeleven/generateKey'
 DELETE_KEY_ENDPOINT = '/oxeleven/rest/oxeleven/deleteKey'
 
 
+def expiration_time_millis(days=365):
+    now = int(time.time())
+    later = int(timedelta(days=days).total_seconds())
+    return (now + later) * 1000
+
+
 def generate_key(ox11server='http://localhost:8190',
                  signature_algorithm='RS512',
-                 expiration_time='216000',
+                 expiration_time=365,
                  token='d-u-m-m-y'):
     url = ox11server + GENERATE_KEY_ENDPOINT
     payload = {'signatureAlgorithm': signature_algorithm,
-               'expirationTime': expiration_time}
+               'expirationTime': expiration_time_millis(expiration_time)}
     headers = {'Authorization': 'Bearer {}'.format(token)}
     r = requests.post(url, data=payload, headers=headers, verify=False)
 
